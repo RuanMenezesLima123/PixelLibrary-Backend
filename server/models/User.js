6e8044ab4ded908acf09d9830bacb0dacc5009b0
RuanMenezesLima123 <ruan.lima07@aluno.ifce.edu.br>
2025-07-15 10:37:05 -0300

    Delete server directory

--- server/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-// Aceita qualquer estrutura de documento
-const userSchema = new mongoose.Schema({}, { strict: false });
-
-// Usa exatamente a coleção existente chamada "users"
-module.exports = mongoose.model('User', userSchema, 'users');
